b2968d2016eb841c60b5d5a141ba067c47da608a
Moon Sung phill <sp9103@naver.com>
2014-12-18 19:05:26 +0900

    -RANSAC implemented
    *Matrix  반환 인터페이스 구현
    *데이터 수집 이후 테스팅 & 디버깅 진행

--- ConsoleApplication1/ConsoleApplication1/BodyCalib.h
+++ ConsoleApplication1/ConsoleApplication1/BodyCalib.h
@@ -6,10 +6,12 @@ public:
 	BodyCalib(void);
 	~BodyCalib(void);
 
+	/*M * X1 = X2
+	X2 is basis space*/
 	void DataStore(cv::Point3f first, cv::Point3f second);
 
 	//RANSAC implement, num : 시행횟수
-	void CalcMatrix(int num);
+	void CalcMatrix(int num, double threshold);
 
 private:
 	cv::Mat RMat;
@@ -22,4 +24,18 @@ private:
 	void SelectRandomNum(int *dst, int sampleCount);
 
 	void printMat(cv::Mat src);
+
+	void CreateMat(int *idxarr, cv::Mat *Mat1, cv::Mat *Mat2);
+
+	// RT한 뒤 오차 반환
+	float CalcDist(cv::Mat RT, int idx);
+
+	//inlier 갯수 반환
+	int CalcInlierCount(cv::Mat RT, float threshold, float *averError);
+
+	//전체 에러 계산
+	void CalcAverError();
+
+	//Matrix 분해
+	void DecomposeRTMat();
 };
